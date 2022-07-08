6a3a4d53a6687f853666277d25a52b6597c2ec20
Tanmay Bhatt <tanmaybhatt.iet@gmail.com>
2022-07-08 11:49:45 +0530

    updated cluster icon

--- src/icons/ClusterIcon.tsx
+++ src/icons/ClusterIcon.tsx
@@ -9,11 +9,12 @@ export const ClusterIcon = () => {
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
     >
-      <rect width="14" height="14" rx="1" fill="#515867" />
-      <rect x="1" y="3.22852" width="12" height="1.61538" fill="white" />
-      <rect x="1" y="8.61328" width="12" height="1.61538" fill="white" />
-      <rect x="1" y="5.91992" width="12" height="1.61538" fill="white" />
-      <rect x="1" y="11.3047" width="12" height="1.61538" fill="white" />
+      <path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M0 1.5C0 0.947715 0.447715 0.5 1 0.5H13C13.5523 0.5 14 0.947715 14 1.5V12.5C14 13.0523 13.5523 13.5 13 13.5H1C0.447716 13.5 0 13.0523 0 12.5V1.5ZM1 3.5H13V5H1V3.5ZM13 8.5H1V10H13V8.5ZM1 6H13V7.5H1V6ZM13 11H1V12.5H13V11Z"
+        fill="#515867"
+      />
     </svg>
   );
 };
