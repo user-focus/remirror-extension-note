984cb52dd55f2693f6ce00be86618b8daeb02c5f
Tanmay Bhatt <tanmaybhatt.iet@gmail.com>
2023-01-26 11:25:20 +0530

    added localhost check

--- src/utils/getNoteKeyFromUrl.ts
+++ src/utils/getNoteKeyFromUrl.ts
@@ -3,10 +3,15 @@
  * @param {string} noteUrl
  * @returns string
  */
-export const getNoteKeyFromNoteUrl = (noteUrl:string = '') => {
-  const noteKey = noteUrl.split('/annotation_tool/event/')[1] || '';
-  if (noteKey.endsWith('/')) {
-    return noteKey.slice(0, -1);
-  }
-  return noteKey;
+export const getNoteKeyFromNoteUrl = (noteUrl: string = "") => {
+    let noteKey;
+    if (noteUrl.includes("/annotation_tool/event/")) {
+        noteKey = noteUrl.split("/annotation_tool/event/")[1] || "";
+    } else if (noteUrl.includes("/note/")) {
+        noteKey = noteUrl.split("/note/")[1] || "";
+    }
+    if (noteKey.endsWith("/")) {
+        return noteKey.slice(0, -1);
+    }
+    return noteKey;
 };
